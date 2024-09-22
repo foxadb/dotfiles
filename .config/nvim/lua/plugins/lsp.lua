17b1f6a39d31d9d749d2dd5c6555ef14d8bb3bdf
Samy Amraoui <sametzy@gmail.com>
2024-09-22 13:07:13 +0200

    feat: add nvim config based on lazyvim

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "lua-language-server",
+        "pyright",
+        "shfmt",
+        "stylua",
+        "typescript-language-server",
+      },
+    },
+  },
+}
