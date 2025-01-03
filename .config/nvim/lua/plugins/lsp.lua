67ea76c35f68c012557c02526d8c1a60e5175e7f
Samy Amraoui <samy.amraoui@amundi.com>
2025-01-03 16:16:33 +0100

    feat: disable nvim inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    }
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
